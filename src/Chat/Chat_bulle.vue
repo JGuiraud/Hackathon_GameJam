8bd5bf7786265bc396a1d6df49a44a209cc58379
Callan <cerveauxallanjean@gmail.com>
2018-05-26 01:18:32 +0200

    bubulle wip

--- src/Chat/Chat_bulle.vue
+++ src/Chat/Chat_bulle.vue
@@ -1,5 +1,9 @@
 <template>
 	<div class="chat-bulle-container">
+		<div class="chat-friend">
+			<span class="chat-bulle">hello world</span>
+			<img class="guy" src="../assets/guy.jpg" alt="">
+		</div>
 		<div class="chat-user">
 			<img class="guy" src="../assets/guy.jpg" alt="">
 			<span class="chat-bulle">hello world</span>
@@ -17,8 +21,8 @@
 	.chat-bulle-container{
 		height: 84vh;
 		display: flex;
-		flex-direction: column;
 		justify-content: flex-end;
+		flex-direction: column;
 	}
 	.chat-user{
 		margin-bottom: 1em;
@@ -34,5 +38,11 @@
 		margin-right: .5em;
   	height: 30px;
   	border-radius: 50%;
+	}
+	.chat-friend{
+	}
+	.chat-friend>.chat-bulle{
+		background-color:rgb(68, 190, 199);
+		color:#fff;
 	}
 </style>
